aa3b25e4ab8e891912f30a1540c4109fc02604a9
chaker <luisgas012@gmail.com>
2024-11-05 22:37:45 -0500

    add: backend

---
+++ Proyecto Laboratorio/lab/src/main/java/laboratorio/lab/controllers/CarrerasController.java
@@ -0,0 +1,13 @@
+package laboratorio.lab.controllers;
+
+import laboratorio.lab.services.CarrerasService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CarrerasController {
+    private CarrerasService carrerasService;
+
+    public CarrerasController(CarrerasService carrerasService) {
+        this.carrerasService = carrerasService;
+    }
+}
---
+++ Proyecto Laboratorio/lab/src/test/tests/laboratorio/lab/LaboratoryApplicationTests.java
@@ -0,0 +1,13 @@
+package laboratorio.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
