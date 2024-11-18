cbb01472fba4b575bb387858a8c277110d2c8b5f
Felipe Triviño <141465004+Petriv2004@users.noreply.github.com>
2024-11-17 20:49:48 -0500

    Delete src directory

--- src/main/java/Los_Jsons/sistemas_reservas/repositories/AreaEstudiosRepository.java
+++
@@ -1,7 +0,0 @@
-package Los_Jsons.sistemas_reservas.repositories;
-
-import Los_Jsons.sistemas_reservas.models.AreaEstudios;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AreaEstudiosRepository extends JpaRepository<AreaEstudios,Integer> {
-}
--- src/main/java/Los_Jsons/sistemas_reservas/repositories/CarrerasRepository.java
+++
@@ -1,7 +0,0 @@
-package Los_Jsons.sistemas_reservas.repositories;
-
-import Los_Jsons.sistemas_reservas.models.Carreras;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarrerasRepository extends JpaRepository<Carreras,Integer> {
-}
