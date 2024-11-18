29401a7db88dfd04dc2dbacaa93afdbd2aaedb3a
chaker <luisgas012@gmail.com>
2024-11-17 21:00:10 -0500

    refactor: restructuracion de las carpetas del proyecto

--- src/main/java/Los_Jsons/sistemas_reservas/dtos/EstudiantesDto.java
+++
@@ -1,6 +0,0 @@
-package Los_Jsons.sistemas_reservas.dtos;
-
-import Los_Jsons.sistemas_reservas.models.Carreras;
-
-public record EstudiantesDto(String nombre, int cedula, Carreras carrera) {
-}
--- src/main/java/Los_Jsons/sistemas_reservas/exceptions/ExceptionReserva.java
+++
@@ -1,7 +0,0 @@
-package Los_Jsons.sistemas_reservas.exceptions;
-
-public class ExceptionReserva extends Exception{
-    public ExceptionReserva(String mensaje) {
-        super(mensaje);
-    }
-}
