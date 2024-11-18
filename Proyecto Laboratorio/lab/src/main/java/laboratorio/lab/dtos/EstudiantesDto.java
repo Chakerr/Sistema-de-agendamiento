ad1a3428745f89755959ed0507bdaf08bf855ef9
Felipe Triviño <141465004+Petriv2004@users.noreply.github.com>
2024-11-17 20:49:11 -0500

    Delete Proyecto Laboratorio directory

--- Proyecto Laboratorio/lab/src/main/java/laboratorio/lab/dtos/EstudiantesDto.java
+++
@@ -1,6 +0,0 @@
-package laboratorio.lab.dtos;
-
-import laboratorio.lab.models.Carreras;
-
-public record EstudiantesDto(String nombre, int cedula, Carreras carrera) {
-}
--- Proyecto Laboratorio/lab/src/main/java/laboratorio/lab/exceptions/ExceptionReserva.java
+++
@@ -1,8 +0,0 @@
-package laboratorio.lab.exceptions;
-
-public class ExceptionReserva extends Exception{
-    // Constructor que recibe un mensaje
-    public ExceptionReserva(String mensaje) {
-        super(mensaje); // Llama al constructor de la superclase
-    }
-}
